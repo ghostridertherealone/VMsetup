e37afc4ec786f3c17b36b07e072024c61b50380f
ghostridertherealone <hubertus@botnition.com>
2025-06-06 09:05:58 +0200

    Update template.ps1

--- template.ps1
+++ template.ps1
@@ -1,5 +1,21 @@
-# Define MAC address (12 hex digits, no colons or dashes)
-$NewMAC = "001122AABBCC"  # Customize this
+# Define MAC prefixes for Intel and AMD
+$IntelPrefixes = @("8086CB", "8086E7", "001B21", "0019D1", "001E37", "002586", "0026B6", "002710", "00270E", "0027EB")
+$AMDPrefixes = @("001018", "00E018", "002155", "0050E4", "00D0B7", "001FC6", "0019BB", "001731", "0014A5", "00166F")
+
+# Randomly select between Intel and AMD
+$AllPrefixes = $IntelPrefixes + $AMDPrefixes
+$RandomPrefix = Get-Random -InputObject $AllPrefixes
+
+# Generate random 6-digit suffix (last 3 bytes)
+$RandomSuffix = ""
+for ($i = 0; $i -lt 6; $i++) {
+    $RandomSuffix += "{0:X}" -f (Get-Random -Minimum 0 -Maximum 16)
+}
+
+# Combine prefix and suffix
+$NewMAC = $RandomPrefix + $RandomSuffix
+
+Write-Host "Generated MAC: $NewMAC"
 
 # Get adapter name (assumes it's the one with Internet access)
 $adapter = Get-NetAdapter | Where-Object { $_.Status -eq 'Up' } | Select-Object -First 1
@@ -12,5 +28,6 @@ Set-NetAdapterAdvancedProperty -Name $adapter.Name -DisplayName "Locally Adminis
 
 # Restart the adapter
 Disable-NetAdapter -Name $adapter.Name -Confirm:$false
-Start-Sleep -Seconds 2
 Enable-NetAdapter -Name $adapter.Name
+
+Write-Host "MAC address changed to: $NewMAC"
